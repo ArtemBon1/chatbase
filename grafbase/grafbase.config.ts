5c7d7eb49ca2f1047364edf52000e47ae7f8c07a
Артем <50744314+ArtemBon1@users.noreply.github.com>
2024-05-12 01:44:01 +0300

    Create grafbase.config.ts

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,47 @@
+import { graph, connector, config } from '@grafbase/sdk';
+
+const g = graph.Standalone();
+
+const jwt = connector.JSONWebToken({
+  issuer: 'nextauth',
+  secret: process.env.NEXTAUTH_SECRET
+});
+
+g.auth(jwt, {
+  rules: {
+    private: {
+      allow: {
+        isLoggedIn: true
+      }
+    }
+  }
+});
+
+const github = connector.GraphQL('GitHub', {
+  url: 'https://api.github.com/graphql',
+  headers: (headers) => {
+    headers.set('Authorization', { forward: 'Authorization' });
+  }
+});
+
+g.datasource(github);
+
+const Message = g.model('Message', {
+  username: String,
+  avatar: String,
+  body: String,
+  likes: Int,
+  dislikes: Int
+});
+
+export default config({
+  graph: g,
+  cache: {
+    rules: [
+      {
+        types: ['Query'],
+        maxAge: 60
+      }
+    ]
+  }
+});
